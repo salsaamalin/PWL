075391394f34f82dde6dc7237028cce6d8caf508
salsaamalin <150258851+salsaamalin@users.noreply.github.com>
2024-03-20 21:06:33 +0700

    Add files via upload

---
+++ app/Http/Controllers/BiodataController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+
+    public function show()
+    {
+        return view('TUGAS1.biodata');
+    }
+}
---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>BIODATA</title>
+</head>
+<body>
+    <h1>BIODATA</h1>
+    <p>NIM: 215150400111047</p>
+    <p>Nama: Amalina Zakiyah Salsabila Aribowo</p>
+    <p>Alamat: Sawojajar 2, Malang</p>
+    <p>Hobi: Menonton Film dan Membaca Buku</p>
+</body>
+</html>
